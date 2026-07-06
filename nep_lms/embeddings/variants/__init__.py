625064a50e87d875c4049295e2887a190726b500
Sanjaya Subedi <jangedoo@gmail.com>
2026-07-06 18:04:44 +0200

    remove rag experiment

--- nep_lms/embeddings/variants/__init__.py
+++ nep_lms/embeddings/variants/__init__.py
@@ -1,9 +1,7 @@
 from nep_lms.embeddings.variants.base import BaseSTEmbeddingVariant
 from nep_lms.embeddings.variants.minilm_l6_v3 import MiniLML6V3Variant
-from nep_lms.embeddings.variants.minilm_l6_v3_rag import MiniLML6V3RagVariant
 
 __all__ = [
     "BaseSTEmbeddingVariant",
     "MiniLML6V3Variant",
-    "MiniLML6V3RagVariant",
 ]
